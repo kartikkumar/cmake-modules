82882f737bc8cff9ae5c87fb5e8ea06f88edcf44
Kartik Kumar <me@kartikkumar.com>
2014-10-29 22:06:50 +0100

    Rename CppTemplate module to CppProject.

---
+++ Modules/FindCppProject.cmake
@@ -0,0 +1,31 @@
+# Copyright (c) <year>, <author> (<email>)
+# Distributed under the MIT License.
+# See accompanying file LICENSE or copy at http://opensource.org/licenses/MIT
+
+if (CPP_PROJECT_INCLUDE_DIRS)
+  # in cache already
+  set(CPP_PROJECT_FOUND TRUE)
+else (CPP_PROJECT_INCLUDE_DIRS)
+
+  find_path(CPP_PROJECT_INCLUDE_DIR
+    NAMES
+      signature_CppProject
+    PATHS
+      /usr/include
+      /usr/local/include
+      /opt/local/include
+      /sw/include
+    PATH_SUFFIXES
+  )
+
+  set(CPP_PROJECT_INCLUDE_DIRS
+    ${CPP_PROJECT_INCLUDE_DIR}
+  )
+
+  include(FindPackageHandleStandardArgs)
+  find_package_handle_standard_args(CATCH DEFAULT_MSG CPP_PROJECT_INCLUDE_DIRS)
+
+  # show the CPP_PROJECT_INCLUDE_DIRS variables only in the advanced view
+  mark_as_advanced(CPP_PROJECT_INCLUDE_DIRS)
+
+endif (CPP_PROJECT_INCLUDE_DIRS)
