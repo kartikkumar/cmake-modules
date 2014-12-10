3efdef03006b028ed5fd7e3c52c7e0b217fba6b7
Kartik Kumar <me@kartikkumar.com>
2014-12-11 00:01:34 +0100

    Update CodeCoverage module so that LCOV only extracts the code relating to the project being analysed. The module now writes the extracted coverage data to coverage.info, so that it can be used in conjunction with coveralls-lcov (https://github.com/okkez/coveralls-lcov).

--- Modules/CodeCoverage.cmake
+++ Modules/CodeCoverage.cmake
@@ -1,47 +1,93 @@
-# - Enable Code Coverage
 #
 # 2012-01-31, Lars Bilke
+# - Enable Code Coverage
+#
+# 2013-09-17, Joakim Söderberg
+# - Added support for Clang.
+# - Some additional usage instructions.
 #
 # USAGE:
-# 1. Copy this file into your cmake modules path
+
+# 0. (Mac only) If you use Xcode 5.1 make sure to patch geninfo as described here:
+#      http://stackoverflow.com/a/22404544/80480
+#
+# 1. Copy this file into your cmake modules path.
+#
 # 2. Add the following line to your CMakeLists.txt:
 #      INCLUDE(CodeCoverage)
 #
-# 3. SET(COVERAGE_EXCLUDES 'dir1/*' 'dir2/*')
-# 4. Use the function SETUP_TARGET_FOR_COVERAGE to create a custom make target
-#    which runs your test executable and produces a lcov code coverage report.
+# 3. Set compiler flags to turn off optimization and enable coverage:
+#    SET(CMAKE_CXX_FLAGS "-g -O0 -fprofile-arcs -ftest-coverage")
+#    SET(CMAKE_C_FLAGS "-g -O0 -fprofile-arcs -ftest-coverage")
+#
+# 3. Use the function SETUP_TARGET_FOR_COVERAGE to create a custom make target
+#    which runs your test executable and produces a lcov code coverage report:
+#    Example:
+#    SETUP_TARGET_FOR_COVERAGE(
+#               my_coverage_target  # Name for custom target.
+#               test_driver         # Name of the test driver executable that runs the tests.
+#                                   # NOTE! This should always have a ZERO as exit code
+#                                   # otherwise the coverage generation will not complete.
+#               coverage            # Name of output directory.
+#               )
+#
+# 4. Build a Debug build:
+#    cmake -DCMAKE_BUILD_TYPE=Debug ..
+#    make
+#    make my_coverage_target
+#
 #
 
 # Check prereqs
 FIND_PROGRAM( GCOV_PATH gcov )
 FIND_PROGRAM( LCOV_PATH lcov )
 FIND_PROGRAM( GENHTML_PATH genhtml )
-FIND_PROGRAM( GCOVR_PATH gcovr PATHS ${CMAKE_SOURCE_DIR}/scripts/test)
+FIND_PROGRAM( GCOVR_PATH gcovr PATHS ${CMAKE_SOURCE_DIR}/tests)
 
 IF(NOT GCOV_PATH)
     MESSAGE(FATAL_ERROR "gcov not found! Aborting...")
 ENDIF() # NOT GCOV_PATH
 
-IF(NOT CMAKE_COMPILER_IS_GNUCXX AND NOT "${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang")
-    MESSAGE(FATAL_ERROR "Compiler is not GNU gcc! Aborting...")
+IF(NOT CMAKE_COMPILER_IS_GNUCXX)
+    # Clang version 3.0.0 and greater now supports gcov as well.
+    MESSAGE(WARNING "Compiler is not GNU gcc! Clang Version 3.0.0 and greater supports gcov as well, but older versions don't.")
+
+    IF(NOT "${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang")
+        MESSAGE(FATAL_ERROR "Compiler is not GNU gcc! Aborting...")
+    ENDIF()
 ENDIF() # NOT CMAKE_COMPILER_IS_GNUCXX
 
-IF ( NOT CMAKE_BUILD_TYPE STREQUAL "Debug" )
-  MESSAGE( WARNING "Code coverage results with an optimised (non-Debug) build may be misleading" )
+SET(CMAKE_CXX_FLAGS_COVERAGE
+    "-g -O0 --coverage -fprofile-arcs -ftest-coverage"
+    CACHE STRING "Flags used by the C++ compiler during coverage builds."
+    FORCE )
+SET(CMAKE_C_FLAGS_COVERAGE
+    "-g -O0 --coverage -fprofile-arcs -ftest-coverage"
+    CACHE STRING "Flags used by the C compiler during coverage builds."
+    FORCE )
+SET(CMAKE_EXE_LINKER_FLAGS_COVERAGE
+    ""
+    CACHE STRING "Flags used for linking binaries during coverage builds."
+    FORCE )
+SET(CMAKE_SHARED_LINKER_FLAGS_COVERAGE
+    ""
+    CACHE STRING "Flags used by the shared libraries linker during coverage builds."
+    FORCE )
+MARK_AS_ADVANCED(
+    CMAKE_CXX_FLAGS_COVERAGE
+    CMAKE_C_FLAGS_COVERAGE
+    CMAKE_EXE_LINKER_FLAGS_COVERAGE
+    CMAKE_SHARED_LINKER_FLAGS_COVERAGE )
+
+IF ( NOT (CMAKE_BUILD_TYPE STREQUAL "Debug" OR CMAKE_BUILD_TYPE STREQUAL "Coverage"))
+  MESSAGE( WARNING "Code coverage results with an optimized (non-Debug) build may be misleading" )
 ENDIF() # NOT CMAKE_BUILD_TYPE STREQUAL "Debug"
 
 
-# Setup compiler options
-ADD_DEFINITIONS(-fprofile-arcs -ftest-coverage)
-
-IF(CMAKE_COMPILER_IS_GNUCXX)
-    LINK_LIBRARIES(gcov)
-ELSE()
-    SET(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} --coverage")
-ENDIF()
-
 # Param _targetname     The name of new the custom make target
-# Param _testrunner     The name of the target which runs the tests
+# Param _testrunner     The name of the target which runs the tests.
+#                       MUST return ZERO always, even on errors.
+#                       If not, no coverage report will be created!
 # Param _outputname     lcov output is generated as _outputname.info
 #                       HTML report is generated in _outputname/index.html
 # Optional fourth parameter is passed as arguments to _testrunner
@@ -66,13 +112,12 @@ FUNCTION(SETUP_TARGET_FOR_COVERAGE _targetname _testrunner _outputname)
         COMMAND ${_testrunner} ${ARGV3}
 
         # Capturing lcov counters and generating report
-        COMMAND ${LCOV_PATH} --directory . --capture --output-file ${_outputname}.info
-        COMMAND ${LCOV_PATH} --remove ${_outputname}.info ${COVERAGE_EXCLUDES} --output-file ${_outputname}.info.cleaned
-        COMMAND ${GENHTML_PATH} -o ${_outputname} ${_outputname}.info.cleaned
-        COMMAND ${CMAKE_COMMAND} -E remove ${_outputname}.info ${_outputname}.info.cleaned
+        COMMAND ${LCOV_PATH} --directory . --capture --output-file ${_outputname}.info.complete
+        COMMAND ${LCOV_PATH} --extract ${_outputname}.info.complete '${MYPROJ_PATH}/*' --output-file ${_outputname}.info
+        COMMAND ${GENHTML_PATH} -o ${_outputname} ${_outputname}.info
+        COMMAND ${CMAKE_COMMAND} -E remove ${_outputname}.info.complete
 
         WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
-        DEPENDS ${_testrunner}
         COMMENT "Resetting code coverage counters to zero.\nProcessing code coverage counters and generating report."
     )
 
@@ -99,43 +144,16 @@ FUNCTION(SETUP_TARGET_FOR_COVERAGE_COBERTURA _targetname _testrunner _outputname
         MESSAGE(FATAL_ERROR "gcovr not found! Aborting...")
     ENDIF() # NOT GCOVR_PATH
 
-    # Combine excludes to several -e arguments
-    SET(COBERTURA_EXCLUDES "")
-    FOREACH(EXCLUDE ${COVERAGE_EXCLUDES})
-        SET(COBERTURA_EXCLUDES "-e ${EXCLUDE} ${COBERTURA_EXCLUDES}")
-    ENDFOREACH()
+    ADD_CUSTOM_TARGET(${_targetname}
 
-    IF(DEFINED ENV{JENKINS_URL})
-        # Get relative paths
-        STRING(REPLACE $ENV{WORKSPACE} "." CMAKE_RELATIVE_SOURCE_DIR ${CMAKE_SOURCE_DIR})
-        STRING(REPLACE "/gpfs0" "" CMAKE_RELATIVE_SOURCE_DIR ${CMAKE_RELATIVE_SOURCE_DIR})
+        # Run tests
+        ${_testrunner} ${ARGV3}
 
-        ADD_CUSTOM_TARGET(${_targetname}
-
-            # Run tests
-            ${_testrunner} ${ARGV3}
-
-            # Running gcovr
-            COMMAND ${GCOVR_PATH} -x -r ${CMAKE_RELATIVE_SOURCE_DIR} ${COBERTURA_EXCLUDES}
-                -o ${CMAKE_BINARY_DIR}/${_outputname}.xml
-            WORKING_DIRECTORY $ENV{WORKSPACE}
-            DEPENDS ${_testrunner}
-            COMMENT "Running gcovr to produce Cobertura code coverage report for Jenkins."
-        )
-    ELSE()
-        ADD_CUSTOM_TARGET(${_targetname}
-
-            # Run tests
-            ${_testrunner} ${ARGV3}
-
-            # Running gcovr
-            COMMAND ${GCOVR_PATH} -x -r ${CMAKE_SOURCE_DIR} ${COBERTURA_EXCLUDES}
-                -o ${_outputname}.xml
-            WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
-            DEPENDS ${_testrunner}
-            COMMENT "Running gcovr to produce Cobertura code coverage report."
-        )
-    ENDIF()
+        # Running gcovr
+        COMMAND ${GCOVR_PATH} -x -r ${CMAKE_SOURCE_DIR} -e '${CMAKE_SOURCE_DIR}/tests/'  -o ${_outputname}.xml
+        WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
+        COMMENT "Running gcovr to produce Cobertura code coverage report."
+    )
 
     # Show info where to find the report
     ADD_CUSTOM_COMMAND(TARGET ${_targetname} POST_BUILD
