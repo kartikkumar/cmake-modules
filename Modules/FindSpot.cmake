9440c8c0c1a86ef92810f3f8c306e14730cc329c
Kartik Kumar <me@kartikkumar.com>
2015-04-21 16:10:45 +0100

    Update Spot CMake module to search for static/shared library.

--- Modules/FindSpot.cmake
+++ Modules/FindSpot.cmake
@@ -5,10 +5,10 @@
 # Distributed under the MIT License.
 # See accompanying file LICENSE.md or copy at http://opensource.org/licenses/MIT
 
-if(SPOT_INCLUDE_DIRS)
+if (SPOT_INCLUDE_DIRS)
   # in cache already
   set(SPOT_FOUND TRUE)
-else(SPOT_INCLUDE_DIRS)
+else (SPOT_INCLUDE_DIRS)
 
   find_path(SPOT_INCLUDE_DIR
     NAMES
@@ -18,17 +18,36 @@ else(SPOT_INCLUDE_DIRS)
       /usr/local/include
       /opt/local/include
       /sw/include
+      /usr/local
       ${PROJECT_PATH}
       ${EXTERNAL_PATH}
     PATH_SUFFIXES
-      Spot include/Spot
+      spot/include/Spot include/Spot Spot
   )
 
-  if(SPOT_INCLUDE_DIR)
+  find_library(SPOT_LIB_PATH
+    NAMES
+      spot
+    PATHS
+      /usr/include
+      /usr/local/include
+      /opt/local/include
+      /sw/include
+      /usr/local
+      ${PROJECT_PATH}
+      ${EXTERNAL_PATH}
+    PATH_SUFFIXES
+      spot spot/build
+    NO_DEFAULT_PATH
+  )
+
+  if(SPOT_INCLUDE_DIR AND SPOT_LIB_PATH)
     set(SPOT_INCLUDE_DIRS
-      ${SPOT_INCLUDE_DIR}/..
+      ${SPOT_INCLUDE_DIR}
     )
-  endif(SPOT_INCLUDE_DIR)
+    get_filename_component(SPOT_LIBRARY_DIR ${SPOT_LIB_PATH} DIRECTORY)
+    set(SPOT_LIBRARY "spot")
+  endif(SPOT_INCLUDE_DIR AND SPOT_LIB_PATH)
 
   include(FindPackageHandleStandardArgs)
   find_package_handle_standard_args(Spot DEFAULT_MSG SPOT_INCLUDE_DIRS)
@@ -36,4 +55,4 @@ else(SPOT_INCLUDE_DIRS)
   # show the SPOT_INCLUDE_DIRS variables only in the advanced view
   mark_as_advanced(SPOT_INCLUDE_DIRS)
 
-endif(SPOT_INCLUDE_DIRS)
+endif (SPOT_INCLUDE_DIRS)
