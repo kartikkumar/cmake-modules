77447de235fa4316a7f59cacd464796b0b9d38cf
Kartik Kumar <me@kartikkumar.com>
2018-06-06 14:55:18 +0200

    Update copyright statement for integrate CMake module.

--- Modules/Findintegrate.cmake
+++ Modules/Findintegrate.cmake
@@ -1,4 +1,4 @@
-# Copyright (c) 2017 K. Kumar (me@kartikkumar.com)
+# Copyright (c) 2018 K. Kumar (me@kartikkumar.com)
 # Distributed under the MIT License.
 # See accompanying file LICENSE.md or copy at http://opensource.org/licenses/MIT
 
