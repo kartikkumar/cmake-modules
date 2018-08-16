ccb7e7c61c3b70c187dba7e28cbc90808c1c640b
Kartik Kumar <me@kartikkumar.com>
2018-08-16 16:38:31 +0200

    Add minor fix to Catch module for setting the include dir path.

--- Modules/FindCatch2.cmake
+++ Modules/FindCatch2.cmake
@@ -21,9 +21,11 @@ else (CATCH2_INCLUDE_DIRS)
       Catch2 Catch2/single_include Catch2/single_include/catch2 Catch2/src/catch2/single_include
   )
 
-  set(CATCH2_INCLUDE_DIRS
-    ${CATCH2_INCLUDE_DIR}/..
-  )
+  if(CATCH2_INCLUDE_DIR)
+    set(CATCH2_INCLUDE_DIRS
+        ${CATCH2_INCLUDE_DIR}/..
+    )
+  endif(CATCH2_INCLUDE_DIR)
 
   include(FindPackageHandleStandardArgs)
   find_package_handle_standard_args(Catch2 DEFAULT_MSG CATCH2_INCLUDE_DIRS)
